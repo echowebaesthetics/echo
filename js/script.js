6fbed9b25b7c1c8267d2746f63b7ae0773b1d287
Nuutti <nuutti@nortunen.com>
2024-10-16 20:08:13 +0000

    1.0.1

--- js/script.js
+++ js/script.js
@@ -75,30 +75,10 @@ let elements = document.querySelectorAll(".item");
 new LoopingElement(elements[0], 0, 0.08);
 new LoopingElement(elements[1], -100, 0.08);
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
 
-const hamMenu = document.querySelector(".ham-menu");
-const open = document.querySelector(".open");
-const close = document.querySelector(".close");
+gsap.registerPlugin(TextPlugin);
+gsap.defaults({ease: "none"});
 
-const offScreenMenu = document.querySelector(".off-screen-menu");
-
-open.addEventListener("click", () => {
-    hamMenu.classList.toggle("active-nav");
-    open.classList.toggle("active");
-    close.classList.toggle("active");
-});
-
-close.addEventListener("click", () => {
-    hamMenu.classList.toggle("active-nav");
-    open.classList.toggle("active");
-    close.classList.toggle("active");
-});
+const tl = gsap.timeline({repeat:Infinity, repeatDelay:1, yoyo:true});
+tl.to("h1 span", {duration: 3, text:"echo"})
